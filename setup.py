b8ef76cfafe70c29843afab4f0f8bfa75a77ca97
Timothée Poisot <t.poisot@gmail.com>
2014-06-08 14:11:46 -0400

    Python install thingies

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+        name='tammy',
+        version='0.0.1',
+        description='Bibliographic management the Python way',
+        author='Timothée Poisot',
+        author_email='t.poisot@gmail.com',
+        url='http://tammy.readthedocs.org/en/latest/',
+        packages=['tammy']
+        )
