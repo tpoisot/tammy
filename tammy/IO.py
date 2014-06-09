309d734651762e8157ab49e0c3752f19ec913c65
Timothée Poisot <t.poisot@gmail.com>
2014-06-09 16:07:07 -0400

    YAML serializer

--- tammy/IO.py
+++ tammy/IO.py
@@ -1,8 +1,13 @@
 from os.path import join
 import json
+import yaml
 
 def export_citeprocjson(records, path):
-    with open(join(path, 'library.json'), 'w') as libfile:
+    with open(join(path, 'default.json'), 'w') as libfile:
         json.dump(records, libfile)
 
-serializer = {'citeproc-json': export_citeprocjson}
+def export_citeprocyaml(records, path):
+    with open(join(path, 'default.yaml'), 'w') as libfile:
+        yaml.dump(records, libfile)
+
+serializer = {'citeproc-json': export_citeprocjson, 'citeproc-yaml': export_citeprocyaml}
