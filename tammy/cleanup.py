7ef23e1d1b122485883e4da9be67ea8df35f39cb
Timothée Poisot <t.poisot@gmail.com>
2014-06-09 15:36:36 -0400

    Flattens some fields to work with pandoc-citeproc

--- tammy/cleanup.py
+++ tammy/cleanup.py
@@ -4,6 +4,9 @@ def clean_all(record):
     conditions to catch all errors I have noticed.
     """
     record = clean_fields(record)
+    for arrayed in ['ISSN']:
+        if arrayed in record:
+            record = clean_arrayed(record, arrayed)
     return record
 
 def clean_fields(record):
@@ -13,3 +16,24 @@ def clean_fields(record):
         record['type'] = 'article-journal'
     return record
 
+def clean_arrayed(record, field):
+    """ Converts arrayed fields for use in pandoc-citeproc
+
+    This is quite a ugly thing to do but pandoc-citeproc
+    has very strict preconceptions of how some fields should
+    be formatted, so for the moment I must comply with it
+
+    Args:
+        record: the ``dict`` representation
+        field: the name of the field to update
+
+    Returns:
+        record with ``field`` flattened and joined by ``,``
+
+    """
+    if isinstance(record[field], list):
+        if len(record[field]) == 1 :
+            record[field] = record[field][0]
+        else :
+            record[field] = ','.join(map(str, record[field]))
+    return record
