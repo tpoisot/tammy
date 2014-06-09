2e9314dae015a0a654b6e58e83138f4f999eaddc
Timothée Poisot <t.poisot@gmail.com>
2014-06-09 11:15:39 -0400

    Updated test suite for from_peerj

--- tests/services.py
+++ tests/services.py
@@ -8,9 +8,15 @@ import tammy
 
 class PeerJ(unittest.TestCase):
     def test_pubtype(self):
-        self.assertRaises(ValueError, tammy.from_peerj(200, 'paper'))
+        self.assertRaises(ValueError, tammy.from_peerj, 200, 'paper')
     def test_argtype(self):
-        self.assertRaises(TypeError, tammy.from_peerj(['200'], 'article'))
+        self.assertRaises(TypeError, tammy.from_peerj, ['200'], 'article')
+    def test_works_for_paper(self):
+        test_paper = tammy.from_peerj(251, 'article')
+        assert test_paper[u'author'][0][u'family'] == u'Poisot'
+    def test_works_for_preprint(self):
+        test_paper = tammy.from_peerj(50, 'preprint')
+        assert test_paper[u'author'][0][u'family'] == u'Poisot'
 
 def main():
     if sys.version_info[1] < 7 :
